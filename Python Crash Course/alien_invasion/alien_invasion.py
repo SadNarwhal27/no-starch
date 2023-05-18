00ac8b0ed59a1ce784cb95307601eb7c92440cd1
SadNarwhal27 <schallt2015@gmail.com>
2023-05-18 18:20:46 -0400

    Finished Ch 14

--- Python Crash Course/alien_invasion/alien_invasion.py
+++ Python Crash Course/alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -20,8 +21,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
-        # Create an instance to store game statistics
+        # Create an instance to store game statistics and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -67,6 +69,8 @@ class AlienInvasion:
         if button_clicked and not self.game_active:
             # Reset the game statistics.
             self._start_game()
+            self.sb.prep_score()
+            self.sb.prep_ships()
         
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
@@ -128,12 +132,27 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.check_high_score()
+
         if not self.aliens:
-            # Destroy existing bullets and create new fleet.
-            self.bullets.empty()
-            self._create_fleet()
-            self.settings.increase_speed()
+            self._new_level()
     
+    def _new_level(self):
+        """Starts a new level of the game"""
+        # Destroy existing bullets and create new fleet.
+        self.bullets.empty()
+        self._create_fleet()
+        self.settings.increase_speed()
+
+        # Increase level
+        self.stats.level += 1
+        self.sb.prep_level()
+
     def _update_aliens(self):
         """Checks fleet edges, then updates positions."""
         self._check_fleet_edges()
@@ -195,8 +214,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Removes all active bullets and aliens.
             self.bullets.empty()
@@ -221,6 +241,9 @@ class AlienInvasion:
 
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.game_active:
--- Python Crash Course/alien_invasion/settings.py
+++ Python Crash Course/alien_invasion/settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.ship_limit = 1
 
         # Bullet settings
-        self.bullet_width = 300
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
@@ -23,6 +23,9 @@ class Settings:
         # Game speed scaling
         self.speedup_scale = 1.1
 
+        # Point value scaling
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -32,8 +35,13 @@ class Settings:
         self.alien_speed = 1.0
         self.fleet_direction = 1 # 1 represents right; -1 left
 
+        # Scoring settings
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
