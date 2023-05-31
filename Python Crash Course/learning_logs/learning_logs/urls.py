99bceac870800a9ad299eb693e28eda7a82e9adb
SadNarwhal27 <schallt2015@gmail.com>
2023-05-31 11:52:14 -0400

    Finished Crash Course Ch 18

--- Python Crash Course/learning_logs/learning_logs/urls.py
+++ Python Crash Course/learning_logs/learning_logs/urls.py
@@ -7,4 +7,10 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
