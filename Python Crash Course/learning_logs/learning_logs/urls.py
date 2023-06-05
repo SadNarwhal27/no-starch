d6ca4584fc31de86b28d2a5b19ed85abc980d8d3
SadNarwhal27 <schallt2015@gmail.com>
2023-06-05 18:11:47 -0400

    Finished everything up to account set up in ch 19

--- Python Crash Course/learning_logs/learning_logs/urls.py
+++ Python Crash Course/learning_logs/learning_logs/urls.py
@@ -13,4 +13,13 @@ urlpatterns = [
 
     # Detail page for a single topic.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Page for adding a new entry.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Page for editing an etry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
