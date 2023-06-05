d890e389b3272880af1e0382e1580f8a5e41ab2e
SadNarwhal27 <schallt2015@gmail.com>
2023-06-05 18:34:00 -0400

    Crash Course Ch 19 - Started user accounts

--- Python Crash Course/learning_logs/ll_project/settings.py
+++ Python Crash Course/learning_logs/ll_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     
     # My apps.
     'learning_logs',
+    'accounts',
 
     # Default django apps.
     'django.contrib.admin',
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
