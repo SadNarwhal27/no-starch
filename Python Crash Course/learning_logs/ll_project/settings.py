dcb5b77b7adab8e963891defcb70663676694a2d
SadNarwhal27 <schallt2015@gmail.com>
2023-06-06 12:27:19 -0400

    Crash Course - Finished ch 19

--- Python Crash Course/learning_logs/ll_project/settings.py
+++ Python Crash Course/learning_logs/ll_project/settings.py
@@ -131,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # My settings.
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
